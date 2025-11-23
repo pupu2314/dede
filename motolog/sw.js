1f23c5a01324af1b6af7591014d4289975d64b47
TaiXiong <pupu2314@hotmail.com>
2025-11-23 21:02:32 +0800

    Create sw.js

---
+++ motolog/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'motolog-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './motolog.css',
+  './motolog.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 安裝 Service Worker 並快取靜態資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching all assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 啟用 Service Worker 並清理舊快取
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[Service Worker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 攔截網路請求：有快取就用快取，沒快取才上網
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
