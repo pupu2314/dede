f4d3772fe3033709f89178e0976fd3eec3244244
TaiXiong <pupu2314@hotmail.com>
2026-03-22 19:56:29 +0800

    Update sw.js

--- motolog/sw.js
+++ motolog/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'motolog-v16.1'; // 記得更新版號
+const CACHE_NAME = 'motolog-v16.0'; // 每次更新都要改這個版號
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -9,36 +9,37 @@ const ASSETS_TO_CACHE = [
   './icon-512.png'
 ];
 
+// 安裝 Service Worker 並快取靜態資源
 self.addEventListener('install', (event) => {
-  // 強制讓新安裝的 SW 進入 active 狀態 (配合自動更新通知)
-  self.skipWaiting();
+  // 強制讓新安裝的 Service Worker 進入 active 狀態，不要停在 waiting
+  self.skipWaiting(); 
   
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching all assets');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
+// 啟用 Service Worker 並清理舊快取
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('[Service Worker] Removing old cache', key);
             return caches.delete(key);
           }
         })
       );
     }).then(() => {
-      console.log('[Service Worker] Claiming clients');
+      // 激活後立即接管所有頁面，並發送更新通知
       return self.clients.claim();
     })
   );
 });
 
+// 攔截網路請求：有快取就用快取，沒快取才上網
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
