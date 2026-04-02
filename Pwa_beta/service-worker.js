3565b3ca302e8e0a1685780a77073903778fff58
TaiXiong <pupu2314@hotmail.com>
2026-04-02 22:50:38 +0800

    Update service-worker.js

--- Pwa_beta/service-worker.js
+++ Pwa_beta/service-worker.js
@@ -4,7 +4,7 @@
  */
 
 // 升級版號，強制所有用戶端清除舊快取並套用新規則
-const CACHE_NAME = 'price-calculator-v26.4k3'; 
+const CACHE_NAME = 'price-calculator-v26.4l'; 
 const OFFLINE_URL = 'index.html';
 
 const urlsToCache = [
